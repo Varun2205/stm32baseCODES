020682bdb0e608fd417f8c6556d483a2dca9e078
Varun2205 <varun.varma2205@gmail.com>
2025-12-15 19:58:25 +0530

    GPIO INPUT

---
+++ 4_gpio_input/Src/main.c
@@ -0,0 +1,58 @@
+#include "stm32f4xx.h"
+
+
+#define GPIOAEN          (1U<<0)
+#define GPIOCEN          (1U<<2)
+
+
+#define PIN5             (1U<<5)
+#define PIN13            (1U<<13)
+
+#define LED_PIN          PIN5
+#define BTN_PIN          PIN13
+
+
+
+
+int main (void){
+	/*
+	RCC->AHB1ENR |= GPIOAEN;
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &=~ (1U<<11);
+	while (1){
+		GPIOA->BSRR = LED_PIN;
+		for(int i=0;i<100000;i++){}
+
+		GPIOA->BSRR = (1U<<21);
+		for(int i=0;i<100000;i++){}
+	}
+	*/
+
+	//Enable clock access to GPIO A and GPIO C
+	RCC->AHB1ENR |= GPIOAEN;
+	RCC->AHB1ENR |= GPIOCEN;
+
+	//Set PA5 as O/P pin
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &=~ (1U<<11);
+
+	//Set PC13 as I/P pin
+	GPIOC->MODER &=~ (1U<<26);
+	GPIOC->MODER &=~ (1U<<27);
+
+
+	while (1){
+		//Check if button is pressed
+		if (GPIOC->IDR & BTN_PIN){
+			//Turn on the LED
+			GPIOA->BSRR = LED_PIN;
+		}
+		else{
+			//Turn it off
+			GPIOA->BSRR = (1U<<21);
+		}
+	}
+
+
+
+}
